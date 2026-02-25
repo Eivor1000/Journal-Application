d578be5e61f385023815a3f91c8769fc3d7a0b67
Ayush Raj <Ayushraj0990@gmail.com>
2026-02-25 17:38:13 +0530

    .

--- src/main/java/com/Ayush/JournalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/Ayush/JournalApp/controller/JournalEntryControllerV2.java
@@ -1,9 +1,9 @@
 package com.Ayush.JournalApp.controller;
 
-import com.Ayush.JournalApp.Repository.JournalEntryRepository;
 import com.Ayush.JournalApp.entity.JournalEntry;
+import com.Ayush.JournalApp.entity.User;
 import com.Ayush.JournalApp.services.JournalEntryServices;
-import org.apache.coyote.Response;
+import com.Ayush.JournalApp.services.UserServices;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,23 +20,25 @@ public class JournalEntryControllerV2 {
 
     @Autowired
     private JournalEntryServices journalEntryServices;
-    @Autowired
-    private JournalEntryRepository journalEntryRepository;
 
-    @GetMapping
-    public ResponseEntity<?> getAll(){
-        List<JournalEntry> all = journalEntryServices.getAll();
+    @Autowired
+    private UserServices userServices;
+
+    @GetMapping("/{username}")
+    public ResponseEntity<?> getAllJournalEntriesOfUser(@PathVariable String username){
+        User user = userServices.findByUsername(username);
+        List<JournalEntry> all = user.getJournalEntries();
         if(all!= null && !all.isEmpty()){
             return new ResponseEntity<>(all,HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping
-    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry){
+    @PostMapping("/{username}")
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry,@PathVariable String username){
        try{
            myEntry.setDate(LocalDateTime.now());
-           journalEntryServices.saveJournalEntry(myEntry);
+           journalEntryServices.saveJournalEntry(myEntry,username);
            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
        }catch(Exception e){
            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -44,24 +46,32 @@ public class JournalEntryControllerV2 {
     }
 
     @GetMapping("id/{myid}")
-    public ResponseEntity<JournalEntry> Getid(@PathVariable ObjectId myid){
+    public ResponseEntity<JournalEntry> GetJournalById (@PathVariable ObjectId myid){
         Optional <JournalEntry> journalEntry = journalEntryServices.findById(myid);
         return journalEntry.map(entry -> new ResponseEntity<>(entry, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @DeleteMapping("id/{myid}")
-    public ResponseEntity<JournalEntry> Deleteid(@PathVariable ObjectId myid){
-      journalEntryRepository.deleteById(myid);
-        return new ResponseEntity<JournalEntry>(HttpStatus.NO_CONTENT);
+    @DeleteMapping("id/{username}/{myid}")
+    public ResponseEntity<?> Deleteid(@PathVariable ObjectId myid,@PathVariable String username){
+      journalEntryServices.deleteById(myid,username);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
-    @PutMapping("id/{id}")
-    public ResponseEntity<?> updateJournalbyId(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry) {
+    @PutMapping("id/{username}/{id}")
+    public ResponseEntity<?> updateJournalbyId(
+            @PathVariable ObjectId id,
+            @RequestBody JournalEntry newEntry,
+            @PathVariable String username) {
+    User user = userServices.findByUsername(username);
+    if(user == null){
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
     JournalEntry old = journalEntryServices.findById(id).orElse(null);
     if(old != null){
-        old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
-        old.setContent(newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent());
+        old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle() : old.getTitle());
+        old.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : old.getContent());
         journalEntryServices.saveJournalEntry(old);
         return new ResponseEntity<>(old, HttpStatus.OK);
     }
--- src/main/java/com/Ayush/JournalApp/services/JournalEntryServices.java
+++ src/main/java/com/Ayush/JournalApp/services/JournalEntryServices.java
@@ -2,10 +2,12 @@ package com.Ayush.JournalApp.services;
 
 import com.Ayush.JournalApp.Repository.JournalEntryRepository;
 import com.Ayush.JournalApp.entity.JournalEntry;
+import com.Ayush.JournalApp.entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,8 +17,19 @@ public class JournalEntryServices {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
+    @Autowired
+    private UserServices userServices;
+
     public void saveJournalEntry(JournalEntry journalEntry){
         journalEntryRepository.save(journalEntry);
+    }
+
+    public void saveJournalEntry(JournalEntry journalEntry, String username){
+        User user = userServices.findByUsername(username);
+        journalEntry.setDate(LocalDateTime.now());
+        JournalEntry saved = journalEntryRepository.save(journalEntry);
+        user.getJournalEntries().add(saved);
+        userServices.saveUser(user);
     }
 
     public List<JournalEntry> getAll(){
@@ -27,7 +40,11 @@ public class JournalEntryServices {
         return journalEntryRepository.findById(id);
     }
 
-    public void deleteById(ObjectId id){
+    public void deleteById(ObjectId id,String username){
+
+        User user = userServices.findByUsername(username);
+        user.getJournalEntries().removeIf(x-> x.getId().equals(id));
+        userServices.saveUser(user);
         journalEntryRepository.deleteById(id);
     }
 }
